291441ddffb3219d9c69bc349774e68155d1473b
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-07-24 22:21:07 -0400

    Codes the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import "../styles/navbar.scss";
+import Link from "next/link";
+
+const categories = ["art", "science", "technology", "cinema", "design", "food"];
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar_container">
-        <div className="logo">logo</div>
-        <div className="links">links</div>
+        <div className="logo">
+          <img src="/logo.png" alt="" />
+        </div>
+        <div className="links">
+          {categories.map((cat, idx) => (
+            <Link href={`/?cat=${cat}`} key={idx} className="link">
+              <h6>{cat.toUpperCase()}</h6>
+            </Link>
+          ))}
+          <span>Mohamed</span>
+          <span>Sign out</span>
+          <span className="write">
+            <Link className="link" href="/write">
+              Write
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );

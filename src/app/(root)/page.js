5a024e6a02a206b9637e16f247064dcdc733d853
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-08-02 21:44:19 -0400

    Removes useContext from root page

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -1,19 +1,7 @@
 "use client";
 import "./page.scss";
 import Home from "@/components/Home";
-import { UserContext } from "@/utilities/UserContext";
-import { useRouter } from "next/navigation";
-import { Router } from "next/router";
-import { useContext, useEffect } from "react";
 export default function page() {
-  const { currentUser } = useContext(UserContext);
-  const router = useRouter();
-  console.log(currentUser);
-
-  useEffect(() => {
-    if (!currentUser) return router.push("/signin");
-  }, []);
-
   return (
     <main className="app">
       <Home />

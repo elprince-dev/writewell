39dd9b627317a27f30fec1ab98bb8cfb6d653f01
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-08-06 22:11:00 -0400

    Adds middleware to signin route

--- src/app/api/auth/signin/route.js
+++ src/app/api/auth/signin/route.js
@@ -3,33 +3,56 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { serialize } from "cookie";
 
+// Function to add CORS headers
+function addCorsHeaders(response) {
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+}
+
+export async function OPTIONS() {
+  const headers = new Headers();
+  addCorsHeaders({ headers });
+  return new Response(null, { headers });
+}
+
 export async function POST(req) {
   const request = await req.json();
 
-  //check if user exists
+  // Check if user exists
   const q = "SELECT * FROM users WHERE username = ?";
   const data = await query({
     query: q,
     values: [request.username],
   });
   if (data.length === 0) {
-    return new Response(JSON.stringify("User not found!"), {
+    const response = new Response(JSON.stringify("User not found!"), {
       status: 404,
     });
+    addCorsHeaders(response);
+    return response;
   }
 
-  //verify password
+  // Verify password
   const isPasswordCorrect = bcrypt.compareSync(
     request.password,
     data[0].password
   );
   if (!isPasswordCorrect) {
-    return new Response(JSON.stringify("Invalid password!"), {
+    const response = new Response(JSON.stringify("Invalid password!"), {
       status: 401,
     });
+    addCorsHeaders(response);
+    return response;
   }
 
-  //generate JWT token
+  // Generate JWT token
   const token = jwt.sign({ id: data[0].id }, process.env.JWT_SECRET);
 
   const { password, ...other } = data[0];
@@ -46,6 +69,9 @@ export async function POST(req) {
       secure: true, // process.env.NODE_ENV === "production",
     })
   );
+
+  // Add CORS headers
+  addCorsHeaders({ headers });
 
   return new Response(JSON.stringify(other), {
     status: 200,
